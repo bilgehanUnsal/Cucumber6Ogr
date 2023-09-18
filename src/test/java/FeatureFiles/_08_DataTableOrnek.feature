2384f0d73a78ff0b78ed44800472b98650f3b9a7
bilgehanUnsal <bilgehanunsal@outlook.com.tr>
2023-09-18 21:41:46 +0300

    DataTable

--- src/test/java/FeatureFiles/_08_DataTableOrnek.feature
+++ src/test/java/FeatureFiles/_08_DataTableOrnek.feature
@@ -3,3 +3,15 @@ Feature: DataTable Ornek
   Scenario: Users List
     When write username "ismet"
     And Write username and Password "ismet" and "1234"
+
+    And Write username as DataTable
+    | bilge  |
+    | mehmet |
+    | ahmet  |
+    | ayse   |
+
+    And Write username and Password as DataTable
+    | bilge  | 1234 |
+    | mehmet | 4567 |
+    | ayse   | 7890 |
+    | fatma  | 1234 |
---
+++ src/test/java/StepDefinitions/_04_DataTableOrnek.java
@@ -0,0 +1,38 @@
+package StepDefinitions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+
+public class _04_DataTableOrnek {
+    @When("write username {string}")
+    public void writeUsername(String username) {
+        System.out.println("username = " + username);
+    }
+
+    @And("Write username and Password {string} and {string}")
+    public void writeUsernameAndPasswordAnd(String username, String password) {
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+    }
+
+    @And("Write username as DataTable")
+    public void writeUsernameAsDataTable(DataTable userlar) {
+        List<String> listUserlar = userlar.asList(String.class);
+
+        for (String user : listUserlar) {
+            System.out.println("user = " + user);
+        }
+    }
+
+    @And("Write username and Password as DataTable")
+    public void writeUsernameAndPasswordAsDataTable(DataTable kullaniciVeSifreler) {
+        List<List<String>> listUserandPasses = kullaniciVeSifreler.asLists(String.class);
+
+        for (int i = 0; i < listUserandPasses.size(); i++) {
+            System.out.println("List Itemlerı = " + listUserandPasses.get(i).get(0) + " " + listUserandPasses.get(i).get(1));
+        }
+    }
+}
