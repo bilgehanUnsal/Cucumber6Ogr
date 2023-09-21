276eb8f89c49c67aab9071364abe430b925014f6
bilgehanUnsal <bilgehanunsal@outlook.com.tr>
2023-09-21 21:45:32 +0300

    Extent report son

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -3,6 +3,9 @@ package StepDefinitions;
 import Utilities.GWD;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -12,7 +15,14 @@ public class Hooks {
     }
 
     @After
-    public void after(){
+    public void after(Scenario senaryo){
+
+        if (senaryo.isFailed()){
+            TakesScreenshot ts = ((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
+
         GWD.quitDriver();
         // System.out.println("Senaryo bitti mi?");
         // doğru çalışırsa burada quiti çağıracağım
